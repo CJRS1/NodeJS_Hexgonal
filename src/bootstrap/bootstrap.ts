dd85eff7bfe1c894f983957898d09d372ccb9544
CJRS1 <reyes.christian@pucp.pe>
2023-11-03 11:53:54 -0500

    Dependencies were installed in the Node.js project.

--- src/bootstrap/bootstrap.ts
+++
@@ -1,8 +0,0 @@
-/* Se puede usar este o el de interface.ts da igual */
-export default abstract class Bootstrap {
-    abstract initialize(): Promise<boolean | Error>;
-}
-
-export abstract class DatabaseListen extends Bootstrap{
-    abstract listen():void;
-}
--- src/drivers/interface/driver.route.ts
+++
@@ -1,9 +0,0 @@
-import express, {Response, Request} from 'express';
-
-const routeDriver = express.Router();
-
-routeDriver.get("/", (req: Request, res: Response) => {
-    res.send("driver");
-})
-
-export default routeDriver;
